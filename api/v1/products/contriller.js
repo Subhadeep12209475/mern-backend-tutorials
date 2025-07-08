28d1dd588d6dcd2397b0ae6ef7a8732874afb694
Subhadeep Mukherjee <subhadeepmukherjee2019@gmail.com>
2025-07-08 13:48:17 +0530

    Day17

--- api/v1/products/contriller.js
+++ api/v1/products/contriller.js
@@ -1,6 +1,39 @@
-const createProductController = (req,res)=>{
-  console.log("creating product....");
-  console.log(req.body);
+const createProductController = async(req,res)=>{
+  try{
+    const data= req.body;
+    console.log("creating product...",data);
+    await product.create(data);
+    
+    let product=null;
+    try{
+      product = await product.create(data);
+    }catch(err){
+      console.log("Error while creating product");
+      res.status(400);
+      res.json({
+        isSuccess:false,
+        message:`Err:${err.message}`,
+        data:{},
+      })
+    }
+    res.status(201);
+    res.json({
+      isSuccess:true,
+      message:`product created`,
+      data:{
+        product:newProduct,
+      }
+    })
+  }
+  catch(err){
+    console.log("Error in createProductController");
+    res.status(501).json({
+      isSuccess:false,
+      message: "internal Server error",
+      data:{}
+    })
+
+  }
 };
 
 module.exports={createProductController};
